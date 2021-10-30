2dc2d3bc736f31ec98e68a8f3b8d2244cdb1645f
john katua <johnkatua99@gmail.com>
2021-10-30 11:31:15 +0300

    added an icon that users can click to be redirected to the homepage

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import './header.css';
+import { ArrowLeft } from 'react-feather';
+
 
 const Header = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  const redirect = () => {
+    return history.push('/')
+  }
+
   return (
     <div className="header">
-      John Katua. 🇰🇪
+      {location.pathname === "/" ? null : (
+        <div className="header--icon" onClick={redirect}>
+          <ArrowLeft />
+        </div>
+      )}
+      <span className="header--details">John Katua. 🇰🇪</span>
     </div>
-  )
+  );
 }
 
 export default Header;
